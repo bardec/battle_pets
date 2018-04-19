9607fc0e70035539ca055346de2c4844e5a8262e
Caleb Barde <calebbarde@gmail.com>
2018-04-19 13:55:18 -0500

    move pet api config into a yaml

--- app/lib/pet_api/client.rb
+++ app/lib/pet_api/client.rb
@@ -2,20 +2,26 @@ require 'faraday'
 
 module PetApi
   class Client
-    URL = "https://wunder-pet-api-staging.herokuapp.com"
-    API_KEY = "UXhygiNEP1vYvRuPA4EluyJxLnscJ6uerTsUlnObFUqKxoyCnN"
-
     def self.get(pet_id:)
+      new.get(pet_id: pet_id)
+    end
+
+    def initialize(config: Rails.application.config_for(:pet_api))
+      @url = config.fetch('url')
+      @api_key = config.fetch('api_key')
+    end
+
+    def get(pet_id:)
       conn.get("/pets/#{pet_id}")
     end
 
     private
+    attr_reader :url, :api_key
 
-    def self.conn
-      Faraday.new(url: URL) do |faraday|
+    def conn
+      Faraday.new(url: url) do |faraday|
         faraday.adapter Faraday.default_adapter
-        #TODO don't leave this
-        faraday.headers["X-Pets-Token"] = API_KEY
+        faraday.headers["X-Pets-Token"] = api_key
         faraday.headers['Content-Type'] = 'application/json'
       end
     end
